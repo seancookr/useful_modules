0a59dd6f41e20202b43af8b3f2ca9979c4503646
seancookr <seancookr@gmail.com>
2014-08-07 20:06:02 -0700

    add filterable module

---
+++ filterable.rb
@@ -0,0 +1,25 @@
+module Filterable
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def filter(filtering_params)
+      results = self.where(nil)
+      filtering_params.each do |key, value|
+        results = results.public_send(key, value) if value.present?
+      end
+      results
+    end
+  end
+end
+
+#def index
+  #@products = Product.filter(params.slice(:status, :location, :starts_with))
+#end
+
+#class Product < ActiveRecord::Base
+  #include Filterable
+
+  #scope :status, -> (status) { where status: status }
+  #scope :location, -> (location_id) { where location_id: location_id }
+  #scope :starts_with, -> (name) { where("name like ?", "#{name}%")}
+#end
